2590bc2f5e49a23090122f0e6acb337a73d5ec3d
qradimir <qradimir@yandex.ru>
2016-07-10 18:29:46 +0300

    first task done

--- src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Lambda.kt
+++ src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Lambda.kt
@@ -4,39 +4,66 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 interface Lambda {
-    fun toBoundedString() : String = "(" + toString() + ")"
+    fun toBoundedString(): String = "(" + toString() + ")"
+    fun substitute(varSubst: String, subst: Lambda): Lambda = this
+    fun reduce(): Lambda? = null
 }
 
 data class Abstraction(
-        val varName : String,
-        val body : Lambda
-) : Lambda
-{
-    override fun toString() : String = '\\' + varName + '.' + body
-}
+        val varName: String,
+        val body: Lambda
+) : Lambda {
+    override fun toString(): String = '\\' + varName + '.' + body
 
-data class Application(
-        val func : Lambda,
-        val arg : Lambda
-) : Lambda
-{
-    override fun toString() : String = when (func) {
-        is Abstraction -> func.toBoundedString()
-        else -> func.toString()
-    } + when(arg) {
-        is Variable -> arg.toString()
-        else -> arg.toBoundedString()
+    override fun substitute(varSubst: String, subst: Lambda): Lambda =
+            if (varSubst.equals(varName)) this
+            else Abstraction(varName, body.substitute(varSubst, subst))
+
+    override fun reduce(): Lambda? {
+        val reduced = body.reduce();
+        return if (reduced == null) null else Abstraction(varName, reduced);
     }
 }
 
-data class Variable(
-        val varName : String
-) : Lambda
-{
-    override fun toString() : String = varName
+data class Application(
+        val func: Lambda,
+        val arg: Lambda
+) : Lambda {
+    override fun toString(): String =
+            when (func) {
+                is Abstraction -> func.toBoundedString()
+                else -> func.toString()
+            } + ' ' +  when (arg) {
+                is Variable -> arg.toString()
+                else -> arg.toBoundedString()
+            }
+
+    override fun substitute(varSubst: String, subst: Lambda): Lambda =
+            Application(func.substitute(varSubst, subst), arg.substitute(varSubst, subst))
+
+    override fun reduce(): Lambda? =
+            if (func is Abstraction)
+                func.body.substitute(func.varName, arg)
+            else {
+                val funcReduced = func.reduce()
+                if (funcReduced == null) {
+                    val argReduced = arg.reduce()
+                    if (argReduced == null) null else Application(func, argReduced)
+                } else
+                    Application(funcReduced, arg)
+            }
 }
 
-fun valueOf(str : String) : Lambda {
+data class Variable(
+        val varName: String
+) : Lambda {
+    override fun toString(): String = varName
+
+    override fun substitute(varSubst: String, subst: Lambda): Lambda =
+            if (varSubst.equals(varName)) subst else this
+}
+
+fun valueOf(str: String): Lambda {
     val lexer = LambdaLexer(ANTLRInputStream(str));
     return LambdaParser(CommonTokenStream(lexer)).expression().ret;
 }
---
+++ src/main/kotlin/ru/itmo/ctddev/sorokin/tt/TaskRunner.kt
@@ -0,0 +1,32 @@
+package ru.itmo.ctddev.sorokin.tt
+
+import kotlin.system.exitProcess
+
+const val jarFileName = "tt-1.0"
+const val usage : String =
+        "usage: java -jar $jarFileName.jar reduce <input-lambda-expression>"
+
+fun assertInput(prec : () -> Boolean) {
+    if (!prec()) {
+        println(usage)
+        exitProcess(-1)
+    }
+}
+
+fun main(args: Array<String>) {
+    assertInput { args.size >= 1 }
+
+    when (args[0]) {
+        "reduce" -> {
+            assertInput { args.size == 2 }
+            var lambda = valueOf(args[1])
+            var reduced = lambda.reduce();
+            while (reduced != null) {
+                lambda = reduced
+                reduced = lambda.reduce()
+            }
+            println(lambda)
+        }
+        else -> assertInput { false }
+    }
+}

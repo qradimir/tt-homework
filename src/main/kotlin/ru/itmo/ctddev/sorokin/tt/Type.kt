3d9a5c4c9185f0896e908266152e3aabd347bb52
qradimir <qradimir@yandex.ru>
2016-09-22 00:09:44 +0300

    type matching added

--- src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Type.kt
+++ src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Type.kt
@@ -3,4 +3,6 @@ package ru.itmo.ctddev.sorokin.tt
 interface Type {
     operator fun contains(typeName: String) : Boolean
     fun substitute(typeName: String, type : Type) : Type
+
+    fun match(other: Type, matchings : MutableMap<String, Type>) : Boolean
 }
--- src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Types.kt
+++ src/main/kotlin/ru/itmo/ctddev/sorokin/tt/Types.kt
@@ -11,6 +11,14 @@ data class TVariable(val typeName : String)
 
     override fun substitute(typeName: String, type: Type): Type =
             if (this.typeName == typeName) type else this
+
+    override fun match(other: Type, matchings: MutableMap<String, Type>) : Boolean{
+        if (typeName in matchings) {
+            return other == matchings[typeName]
+        }
+        matchings[typeName] = other
+        return true
+    }
 }
 
 data class TApplication(val argType : Type, val resType: Type)
@@ -27,6 +35,13 @@ data class TApplication(val argType : Type, val resType: Type)
 
     override fun substitute(typeName: String, type: Type): Type
             = TApplication(argType.substitute(typeName, type), resType.substitute(typeName, type))
+
+    override fun match(other: Type, matchings: MutableMap<String, Type>): Boolean {
+        if (other !is TApplication)
+            return false
+
+        return argType.match(other.argType, matchings) && resType.match(other.resType, matchings)
+    }
 }
 
 fun Type.toBoundedString(): String = "(${toString()})"

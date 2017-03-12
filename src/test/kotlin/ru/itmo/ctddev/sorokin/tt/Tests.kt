615635c1136c49565113ef0e842a8ca90628f5e4
qradimir <rad.sorokin@gmail.com>
2017-03-13 01:32:58 +0300

    migrate tests to junit 5

--- src/test/kotlin/ru/itmo/ctddev/sorokin/tt/Tests.kt
+++ src/test/kotlin/ru/itmo/ctddev/sorokin/tt/Tests.kt
@@ -1,53 +1,59 @@
 package ru.itmo.ctddev.sorokin.tt
 
+import kotlin.test.*
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Test as test
+import org.junit.jupiter.api.TestFactory as testFactory
+
 import ru.itmo.ctddev.sorokin.tt.lambdas.Lambda
 import ru.itmo.ctddev.sorokin.tt.lambdas.reduceFully
 import ru.itmo.ctddev.sorokin.tt.lambdas.valueOf
-import kotlin.test.*
-
-import org.junit.Test as test
 
 class Tests {
 
-    @test
-    fun testParser() {
-        for ((givenLambda, expectedLambda) in getTestData()) {
-            val actual: Lambda
-            try {
-                actual = valueOf(givenLambda).resolve(getGlobalScope())
-            } catch (e: Exception) {
-                fail("Got exception '${e.message}' on parsing '$givenLambda'")
-            }
-            assertEquals(actual, expectedLambda, "Fails on parsing '$givenLambda'")
+    val testData: Iterator<LambdaTest>
+        get() = getTestData().iterator()
+
+    fun testName(prefix: String) = { test: LambdaTest -> "$prefix \" ${test.asString} \"" }
+
+    fun tests(testNamePrefix: String, test: (LambdaTest) -> Unit) =
+            DynamicTest.stream(testData, testName(testNamePrefix), test)
+
+    @testFactory
+    fun testParser() = tests("parse") { (givenLambda, expectedLambda) ->
+        val actual: Lambda
+        try {
+            actual = valueOf(givenLambda).resolve(getGlobalScope())
+        } catch (e: Exception) {
+            fail("Got exception '${e.message}' on parsing '$givenLambda'")
         }
+        assertEquals(actual, expectedLambda, "Fails on parsing '$givenLambda'")
+
     }
 
-    @test
-    fun testReduce() {
-        for ((str, lambda, reducedLambda) in getTestData()) {
-            val actual = lambda.reduceFully()
-            assertEquals(actual, reducedLambda, "Fails on reducing '$lambda'")
-        }
+    @testFactory
+    fun testReduce() = tests("reduce") { (_, lambda, reducedLambda) ->
+        val actual = lambda.reduceFully()
+        assertEquals(actual, reducedLambda, "Fails on reducing '$lambda'")
     }
 
-    @test
-    fun testTypeDeduction() {
-        for ((str, lambda, reduced, expectedType, expectedContext) in getTestData()) {
-            val type = getTypeManager().resolve(lambda)
 
-            if (expectedType == null) {
-                assertNull(type, "Type of '$lambda' should not be resolved")
-            } else {
-                val typeNN = assertNotNull(type, "Type of '$lambda' should be resolved")
-                expectedType.concrete()
-                assertTrue(expectedType unifyWith typeNN, "Deduced type of '$lambda' mismatched")
+    @testFactory
+    fun testTypeInference() = tests("inference") { (_, lambda, _, expectedType, expectedContext) ->
+        val type = getTypeManager().resolve(lambda)
 
-                for ((variable, expected) in expectedContext) {
-                    val actual = assertNotNull(getTypeManager().typeFor(variable),
-                            "Type of variable ($variable should be resolved")
-                    expected.concrete()
-                    assertTrue(expected.unifyWith(actual), "Deduced variable ($variable) type mismatched")
-                }
+        if (expectedType == null) {
+            assertNull(type, "Type of '$lambda' should not be resolved")
+        } else {
+            val typeNN = assertNotNull(type, "Type of '$lambda' should be resolved")
+            expectedType.concrete()
+            assertTrue(expectedType unifyWith typeNN, "Deduced type of '$lambda' mismatched")
+
+            for ((variable, expected) in expectedContext) {
+                val actual = assertNotNull(getTypeManager().typeFor(variable),
+                        "Type of variable ($variable should be resolved")
+                expected.concrete()
+                assertTrue(expected.unifyWith(actual), "Deduced variable ($variable) type mismatched")
             }
         }
     }
